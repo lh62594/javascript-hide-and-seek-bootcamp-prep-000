d29edb582c60226d77b987c6f2be4df351d1f6e3
breezy-karma-0220 <lh62594@yahoo.com>
2018-11-13 14:21:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+  var lis = document.querySelectorAll('.ranked-list');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + parseInt(n)).toString();
+  }
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
