a7b088a6dccace1d6e8725f5eb8179fe03ea6436
breezy-karma-0220 <lh62594@yahoo.com>
2018-11-13 14:14:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ function getFirstSelector(selector) {
 function nestedTarget() {
   return document.querySelector('#nested .target');
 }
+
+function increaseRankBy(n) {
+  var lis = document.querySelector('.ranked-list');
+  for (var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = (i + parseInt(n)).toString();
+  }
+}
