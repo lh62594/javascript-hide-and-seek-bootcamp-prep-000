cd4413bb068370a9422b9d742f9b9562d2075177
breezy-karma-0220 <lh62594@yahoo.com>
2018-11-13 14:03:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
 
+function nestedTarget() {
+  return document.getElementById('nested').querySelectorAll('div.target')
+}
